b0be2395cb198aa5f9c5609145c2eec4aa224d4a
zee <zperezcanals@gmail.com>
2018-05-27 23:18:43 -0400

    portfolio modal done

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// $(document).ready(function(){
+// 	$("#myModal").modal('toggle')
+
+// });
+
+// // $(document).ready(function() {
+// //  // executes when HTML-Document is loaded and DOM is ready
+// // console.log("document is ready");
+  
+  
+//   $('.btn').click(function(e){
+//     e.preventDefault();
+//    $("#myModal").modal('toggle')
+  
+//   });
+ 
